fb4b4d4ed9f99289769b58e6d76f48e03d8cab74
iPadDerek <derek.schrag@gmail.com>
2020-03-31 19:46:17 -0500

    Fixing test

--- calc/button.c
+++ calc/button.c
@@ -6,12 +6,8 @@ int main()
 {
   printit("The button.c file is...");
   printf("modified\n");
-<<<<<<< HEAD
   printf("modified thrice \n");
   printf("which is a third time\n");
-=======
-  printf("modified thrice \n");
->>>>>>> 2a4a2785afd994ec49e6d80e347ee9af1ea124d1
 }
 
 void printit(char *string)
