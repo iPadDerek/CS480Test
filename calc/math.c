269fa340fe136fb9fd45122f716ab2b33189ee81
iPadDerek <derek.schrag@gmail.com>
2020-03-31 19:10:32 -0500

    Test Run: 16X

--- calc/math.c
+++ calc/math.c
@@ -1,6 +1,7 @@
 int main()
 {
     printf("This is math.c");
-    printf("This is a git change"); //This is a change, avoiding conflict
+    printf("This is a git change");
+    printf("No conflict Please");
     return 0;
 }
